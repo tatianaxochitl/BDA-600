b2048c389065ece0dc6d37eaf49f0044c46b0701
Marcos <marcos@hpionline.com>
2022-03-17 09:45:35 -0700

    Downloaded all of the images

--- Stocks.py
+++ Stocks.py
@@ -1,23 +1,28 @@
+# We start by importing all of the modules we think we might need.
+from sklearn.metrics import mean_absolute_percentage_error
 from tracemalloc import start
 import matplotlib.pyplot as plt
+#!pip install yfinance
 import yfinance as yf
+#!pip install pandas
 import pandas as pd
 import numpy as np
 import os
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.seasonal import seasonal_decompose
-from pmdarima.arima import auto_arima
 from statsmodels.tsa.arima.model import ARIMA
 from sktime.forecasting.arima import AutoARIMA
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import math
 
+# Creating a list of the stock/cryptos we want to use
 stockList = ['AAPL', 'TSLA', 'AMZN', 'FB', 'JPM', 'BTC-USD', 'ETH-USD', 'DOGE-USD', "ADA-USD", "SOL-USD"]
 
 # Let us define our start and end time
 startDate = '2018-01-01'
 endDate = '2021-12-31'
 
+# This actually pulls all of the historical data for our analysis
 aapl = yf.Ticker("AAPL").history(start=startDate, end=endDate)
 tsla = yf.Ticker("TSLA").history(start=startDate, end=endDate)
 amzn = yf.Ticker("AMZN").history(start=startDate, end=endDate)
@@ -29,13 +34,14 @@ doge = yf.Ticker("DOGE-USD").history(start=startDate, end=endDate)
 ada = yf.Ticker("ADA-USD").history(start=startDate, end=endDate)
 sol = yf.Ticker("SOL-USD").history(start=startDate, end=endDate)
 
+# Putting all of the stocks/cryotos in one object for reference later
 stockNames = [aapl, tsla, amzn, fb, jpm, btc, eth, doge, ada, sol]
 
+# Adding in the adjusted close column to all of the stocks/cryptos
 for x in stockNames:
     x['Adj Close'] = x['Close'] - x['Dividends']
 
 # This plots the four year Daily Highs for each of the stocks
-
 for i, x in enumerate(stockNames):
     plt.subplot(5, 2, i + 1)
     plt.ylabel('Price in US Dollar')
@@ -45,46 +51,43 @@ plt.savefig('FourYearDailyHigh.png')
 
 # We do the same for the lows
 for i, x in enumerate(stockNames):
-    plt.subplot(len(stockNames), 2, i + 1)
+    plt.subplot(5, 2, i + 1)
     plt.ylabel('Price in US Dollar')
-    x['Low'].plot(figsize=(10, 20), title=f'Four Year Daily Low History: {stockList[i]} ')
+    x['Low'].plot(figsize=(15, 25), title=f'Four Year Daily Low History: {stockList[i]} ')
     plt.tight_layout()
-    break
+plt.savefig('FourYearDailyLow.png')
 
 # And again for the Open and Close
 for i, x in enumerate(stockNames):
-    plt.subplot(len(stockNames), 2, i + 1)
+    plt.subplot(5, 2, i + 1)
     plt.ylabel('Price in US Dollar')
-    x['Open'].plot(figsize=(10, 20), title=f'Four Year Daily Open Price History:{stockList[i]} ')
-    break
+    x['Open'].plot(figsize=(15, 25), title=f'Four Year Daily Open Price History:{stockList[i]} ')
+    plt.tight_layout()
+plt.savefig('FourYearDailyOpen.png')
 
 for i, x in enumerate(stockNames):
-    plt.subplot(len(stockNames), 2, i + 1)
+    plt.subplot(5, 2, i + 1)
     plt.ylabel('Price in US Dollar')
-    x['Close'].plot(figsize=(10, 20), title=f'Four Year Daily Closing Price History: {stockList[i]}')
-    break
+    x['Close'].plot(figsize=(15, 25), title=f'Four Year Daily Closing Price History: {stockList[i]}')
+    plt.tight_layout()
+plt.savefig('FourYearDailyClose.png')
 
 # Volume traded
 for i, x in enumerate(stockNames):
-    plt.subplot(len(stockNames), 2, i + 1)
+    plt.subplot(5, 2, i + 1)
     plt.ylabel('Volume Traded')
-    x['Volume'].plot(figsize=(10, 20), title=f'Four Year History of Daily Volume Traded: {stockList[i]}')
-    break
-
-# High Minus Low
-for i, x in enumerate(stockNames):
-    plt.subplot(len(stockNames), 2, i + 1)
-    plt.ylabel('Price in US Dollar')
-    (x['High'] - x['Low']).plot(figsize=(10, 20), title=f'Four Year Daily High Minus Low Price History: {stockList[i]}')
-    break
+    x['Volume'].plot(figsize=(15, 25), title=f'Four Year History of Daily Volume Traded: {stockList[i]}')
+    plt.tight_layout()
+plt.savefig('VolumeTraded.png')
 
 # Open-Close
 for i, x in enumerate(stockNames):
-    plt.subplot(len(stockNames), 2, i + 1)
+    plt.subplot(5, 2, i + 1)
     plt.ylabel('Price in US Dollar')
-    (x['Open'] - x['Close']).plot(figsize=(10, 20),
+    (x['Open'] - x['Close']).plot(figsize=(15, 25),
                                   title=f'Four Year Daily Open-Close Difference History: {stockList[i]}')
-    break
+    plt.tight_layout()
+plt.savefig('OpenCloseDifference.png')
 
 # Lets Start looking at the moving averages
 # Set the moving average values
@@ -95,50 +98,45 @@ movingAverageDays = [7, 14, 21]
 for mov_avg in movingAverageDays:
     for stock in stockNames:
         c_name = f"Moving Avg for {mov_avg} days"
-        stock[c_name] = stock['Close'].rolling(mov_avg).mean()
+        stock[c_name] = stock['Adj Close'].rolling(mov_avg).mean()
         c2_name = f"Standard Deviation for {mov_avg} days"
-        stock[c2_name] = stock['Close'].rolling(mov_avg).std()
+        stock[c2_name] = stock['Adj Close'].rolling(mov_avg).std()
 
-# Let's plot the moving averages (overlayed) for each stock
-'''
-aapl.plot(y=['Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
-tsla.plot(y=['Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
-amzn.plot(y=['Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
-fb.plot(y=['Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
-jpm.plot(y=['Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
-eth.plot(y=['Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
-btc.plot(y=['Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
-doge.plot(y=['Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
-'''
+aapl.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('AppleMA.png')
 
+tsla.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('TeslaMA.png')
 
-# let's look at the ARIMA Model
-# Testing the a distribution of one of the stocks
-aapl.plot(y=['Close'], figsize=(5, 5), kind='kde')
-plt.close()
-# plt.show()
+amzn.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('AmazonMA.png')
 
-'''
-- The average value in the series is called the level.
-- The increasing or falling value in the series is referred to as the trend.
-- Seasonality is the series’ recurring short-term cycle.
-- The random variance in the series is referred to as noise.
-'''
-# Using the stored information we have from the previous for loop, we can compare for stationarity
-aapl.plot(y=['Adj Close', 'Moving Avg for 14 days', 'Standard Deviation for 14 days'])
-plt.close()
+fb.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('FacebookMA.png')
 
-# plt.show()
+jpm.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('JPMMA.png')
 
+eth.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('EtherumMA.png')
+
+btc.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('BitcoinMA.png')
+
+doge.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('DogeMA.png')
+
+ada.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('ADAMA.png')
+
+sol.plot(y=['Adj Close', 'Moving Avg for 7 days', 'Moving Avg for 14 days', 'Moving Avg for 21 days'])
+plt.savefig('SOLMA.png')
 
 '''NOTE: An increasing rolling mean and standard deviation indicate that the series is not stationary.
 The website that this model is based off of suggests that seasonality and trend may need to be removed to
 undertake time series analysis. We will replicate that deconstruction for one of our stocks'''
 
-# testing the function
-# test_for_stationarity(aapl['Close'])
-
-# We can remove any trend by using the log function
+# We can remove trend by using the log function
 log_aapl = np.log(aapl['Adj Close'])
 
 # Now we can train the ARIMA Model
@@ -153,7 +151,42 @@ plt.ylabel('Closing Prices')
 plt.plot(log_aapl, 'green', label='Train data')
 plt.plot(testData, 'blue', label='Test data')
 plt.legend()
-# plt.show()
+plt.show()
 
-# Modeling
-# Build Model
+forecaster = AutoARIMA(start_p=8, max_p=9, suppress_warnings=True)
+trainData.index = trainData.index.astype(int)
+forecaster.fit(trainData)
+forecaster.summary()
+
+
+def plot_forecast(series_train, series_test, forecast, forecast_int=None):
+
+    mae = mean_absolute_error(series_test, forecast)
+    mape = mean_absolute_percentage_error(series_test, forecast)
+
+    plt.figure(figsize=(12, 6))
+    plt.title(f"MAE: {mae:.2f}, MAPE: {mape:.3f}", size=18)
+    series_train.plot(label="Train", color="b")
+    series_test.plot(label="Test", color="g")
+    forecast.index = series_test.index
+    forecast.plot(label="Forecast", color="r")
+    if forecast_int is not None:
+        plt.fill_between(
+            series_test.index,
+            forecast_int["lower"],
+            forecast_int["upper"],
+            alpha=0.2,
+            color="dimgray",
+        )
+    plt.legend(prop={"size": 16})
+    plt.show()
+
+    return mae, mape
+
+
+test_len = int(len(aapl) * 0.2)
+fh = np.arange(test_len) + 1
+forecast, forecast_int = forecaster.predict(fh=fh, return_pred_int=True, alpha=0.05)
+sun_arima_mae, sun_arima_mape = plot_forecast(
+    trainData, testData, forecast, forecast_int
+)
